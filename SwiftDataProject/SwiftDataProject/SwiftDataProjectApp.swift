6344e321a6b585272692f1c57d25afb5e8983f88
Akash Sheelavant <asheelavant@walmart.com>
2024-11-11 10:06:06 +0530

    Initial project commit.

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Akash Sheelavant on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
